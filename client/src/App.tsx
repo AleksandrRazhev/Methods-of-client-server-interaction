e1ab4890bfd97840701a58bfe9d22bdd97ee1c2f
AleksandrRazhev <alexandrrazh@gmail.com>
2024-06-26 21:25:14 +0300

    client added

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import { styled } from "@mui/material/styles";
+import { Grid, Box, Paper } from "@mui/material";
+
+function App() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <h1>It's working</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default App;
