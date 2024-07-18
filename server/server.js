71982d08b20e7e8bf565235b984b008967295415
AleksandrRazhev <alexandrrazh@gmail.com>
2024-07-18 20:46:09 +0300

    web socket server added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 import express from "express";
 import cors from "cors";
 import { EventEmitter } from "events";
+import { WebSocketServer } from "ws";
 
 import { faker } from "@faker-js/faker";
 
 const HTTP_PORT = 4000;
+const WS_PORT = 2000;
 const app = express();
 const eventEmitter = new EventEmitter();
+const wws = new WebSocketServer(
+  { port: WS_PORT },
+  () => `web socket server is running on port ${WS_PORT}`
+);
+wws.on("connection", (ws) => {
+  let last = 0;
+  ws.on("error", console.error());
+  ws.on("message", (data) => {
+    const { lastUserIndex } = JSON.parse(data);
+    last = lastUserIndex;
+  });
+  eventEmitter.on("add-user", () => {
+    const freshUser = usersDB.slice(last, usersDB.length);
+    last = usersDB.length;
+    const data = JSON.stringify({ users: usersDB, last });
+    ws.send(data);
+  });
+});
 app.use(express.json());
 app.use(cors());
 let usersDB = [];
